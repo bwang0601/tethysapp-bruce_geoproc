c8618ff985f84e7d02bd0611b8f7f2d3a66768f4
bwang0601 <bwang0601@gmail.com>
2019-03-21 14:10:53 -0400

    Initial commit

---
+++ tethysapp/bruce_geoproc/app.py
@@ -0,0 +1,39 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class BruceGeoproc(TethysAppBase):
+    """
+    Tethys app class for Bruce Geoproc.
+    """
+
+    name = 'Bruce Geoproc'
+    index = 'bruce_geoproc:home'
+    icon = 'bruce_geoproc/images/icon.gif'
+    package = 'bruce_geoproc'
+    root_url = 'bruce-geoproc'
+    color = '#2980b9'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='bruce-geoproc',
+                controller='bruce_geoproc.controllers.home'
+            ),
+            UrlMap(
+                name='group',
+                url='bruce-geoproc/group',
+                controller='bruce_geoproc.controllers.group'
+            ),
+        )
+
+        return url_maps
